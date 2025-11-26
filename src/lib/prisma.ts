eecad60b77ef7e2e642d7f6d4a72b4bd0ec92b24
akyrey <stein.dario@gmail.com>
2025-11-26 10:04:13 +0100

    feat: update to use turso

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,14 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+const adapter = new PrismaLibSql({
+  url: process.env.TURSO_DATABASE_URL ?? '',
+  authToken: process.env.TURSO_AUTH_TOKEN,
+})
+export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
