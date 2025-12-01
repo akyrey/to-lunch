89b398a745f1b8f89218edcbd2360a0d3bf6b125
akyrey <stein.dario@gmail.com>
2025-12-01 22:32:35 +0100

    feat: upgrade Prisma to 7.0.1, introduce `prisma.config.ts` for datasource configuration, and update `PrismaLibSql` initialization.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 
 const globalForPrisma = globalThis as unknown as {
     prisma: PrismaClient | undefined
 }
 
 const adapter = new PrismaLibSql({
-  url: process.env.TURSO_DATABASE_URL ?? '',
-  authToken: process.env.TURSO_AUTH_TOKEN,
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
 })
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
